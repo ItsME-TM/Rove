538a8794cca08725a25e291d19d770057aa308ec
ItsME-TM <thushanmalaka123@gmail.com>
2026-05-04 10:34:37 +0530

    feat: implement user auth with browser local storage support

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,6 +3,7 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,7 @@ import { Nav } from "../layout/nav/nav";
   styleUrl: './app.css'
 })
 export class App implements OnInit{
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected title = 'Rove';
   protected members = signal<any>([]);
@@ -18,7 +20,13 @@ export class App implements OnInit{
   async ngOnInit(){
     this.members.set(await this.getMembers());
   }
-
+  
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
   async getMembers() {
     try{
       return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
---
+++ client/src/types/user.ts
@@ -0,0 +1,17 @@
+export type User = {
+    id: string;
+    displayName: string;
+    email: string;
+    token: string;
+    imageUrl?: string;
+}
+
+export type LoginCreds = {
+    email: string;
+    password: string;
+}
+
+export type ResgisterCreds = {
+    email: string;
+    password: string;
+}
