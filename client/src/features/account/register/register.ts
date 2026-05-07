03f070bea397a7ef7260fe2ee2bc01d610727553
ItsME-TM <thushanmalaka123@gmail.com>
2026-05-07 23:32:58 +0530

    feat: enhance registration form with user input fields and integrate member selection

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { RegisterCreds, User } from '../../../types/user';
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  membersFromHome = input.required<User[]>();
+  protected creds = {} as RegisterCreds;
+
+  register(){
+    console.log(this.creds);
+  }
+
+  cancel() {
+    console.log('cancelled');
+  }
+}
