c076c9ae0ab706bd75989ceb1a0231d30f9d6248
ItsME-TM <thushanmalaka123@gmail.com>
2026-05-14 16:10:38 +0530

    feat: enhance app initialization with splash screen and auth guard for route protection

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,44 +1,37 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
-import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
-import { User } from '../types/user';
-import { NgClass } from '@angular/common';
+import { Nav } from '../layout/nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, RouterOutlet],  // Import child components to make them available in this component
+  imports: [Nav, RouterOutlet], // Import child components to make them available in this component
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App implements OnInit{
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
+export class App {
+  // private accountService = inject(AccountService);
+  // private http = inject(HttpClient);
   protected router = inject(Router);
-  protected title = 'Rove';
-  protected members = signal<User[]>([]);  // Signal to hold list of members from API
+  // protected title = 'Rove';
+  // protected members = signal<User[]>([]); // Signal to hold list of members from API
 
-  async ngOnInit(){
-    this.members.set(await this.getMembers());  // Fetch members from API and store in signal
-    this.setCurrentUser();  // Load current user from localStorage
-  }
-  
-  setCurrentUser(){
-    const userString = localStorage.getItem('user');
-    if(!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-  async getMembers() {
-    try{
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
-    }catch(error){
-      console.log(error);
-      throw error;    
-    }
-  }
+  // async ngOnInit() {
+  //   this.members.set(await this.getMembers()); // Fetch members from API and store in signal
+  //   //this.setCurrentUser();  // Load current user from localStorage
+  // }
 
+  // setCurrentUser(){
+  //   const userString = localStorage.getItem('user');
+  //   if(!userString) return;
+  //   const user = JSON.parse(userString);
+  //   this.accountService.currentUser.set(user);
+  // }
+  // async getMembers() {
+  //   try {
+  //     return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
+  //   } catch (error) {
+  //     console.log(error);
+  //     throw error;
+  //   }
+  // }
 }
