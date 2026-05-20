96095924d1bcd8e47385a6dabd9f4acc7b0e014e
ItsME-TM <thushanmalaka123@gmail.com>
2026-05-20 22:16:47 +0530

    feat: implement database migration and seeding in Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -39,4 +39,19 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
+//Seed data
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+
 app.Run();
