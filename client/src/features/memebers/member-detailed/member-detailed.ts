c75f6819e3f8ba04e8a8ed469b78a6efde7537c9
ItsME-TM <thushanmalaka123@gmail.com>
2026-05-27 16:20:33 +0530

    feat: add member profile, photos, and messages components; update routing and member detailed view

--- client/src/features/memebers/member-detailed/member-detailed.ts
+++ client/src/features/memebers/member-detailed/member-detailed.ts
@@ -1,30 +1,56 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import { AsyncPipe } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { Member } from '../../../types/member';
-import { Observable } from 'rxjs';
+import { filter, Observable } from 'rxjs';
 
+  // Signals are synchronous wrappers around values that automatically notify
+  // consumers when their value changes. They're fine-grained and perfect for
+  // local component state that updates immediately (like the title signal in your
+  //  code). You read them with title() and update with .set().
+
+  // Observables represent streams of values over time (like HTTP responses or
+  // route params). They're asynchronous and require subscription to get values
+  // (as you see with member$ using AsyncPipe). They excel at handling events that
+  //  happen over time, like API calls or user actions.
 @Component({
   selector: 'app-member-detailed',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member-detailed.html',
   styleUrl: './member-detailed.css',
 })
-export class MemberDetailed  implements OnInit {
+export class MemberDetailed implements OnInit {
   private memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
   // we created a observable because we want get http response quickly
   // so by using observable without freezing the UI and waiting
   protected member$?: Observable<Member>;
+  private router = inject(Router);
+  protected title = signal<string | undefined>('Profile');
 
   ngOnInit(): void {
     this.member$ = this.loadMember();
+    //component first loads, we set the title to the first child route's title (which is 'Profile' by default)
+    this.title.set(this.route.firstChild?.snapshot?.title);
+    // then we subscribe to router events, and whenever a navigation ends, we update the title to the current child route's title
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe({
+      next: () => {
+        this.title.set(this.route.firstChild?.snapshot?.title);
+      },
+    });
   }
 
   loadMember() {
     const id = this.route.snapshot.paramMap.get('id');
-    if(!id) return;
+    if (!id) return;
     return this.memberService.getMember(id);
   }
 }
