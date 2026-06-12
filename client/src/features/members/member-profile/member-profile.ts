30f167964ba7d9250fc80b6f6612b2e06bb2b05a
ItsME-TM <thushanmalaka123@gmail.com>
2026-06-12 19:19:54 +0530

    feat: implement member profile update functionality, including new DTO, repository method, and guard for unsaved changes

--- client/src/features/members/member-profile/member-profile.ts
+++ client/src/features/members/member-profile/member-profile.ts
@@ -1,4 +1,12 @@
-import { Component, inject, OnDestroy, OnInit, signal, ViewChild } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  inject,
+  OnDestroy,
+  OnInit,
+  signal,
+  ViewChild,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EditableMember, Member } from '../../../types/member';
 import { MemberService } from '../../../core/services/member-service';
@@ -13,16 +21,21 @@ import { ToastService } from '../../../core/services/toast-service';
 })
 export class MemberProfile implements OnInit, OnDestroy {
   @ViewChild('editForm') editForm?: NgForm;
+  @HostListener('window:beforeunload', ['$event']) notify($event: BeforeUnloadEvent) {
+    if (this.editForm?.dirty) {
+      $event.preventDefault();
+    }
+  }
   protected memberService = inject(MemberService);
   private toast = inject(ToastService);
   private route = inject(ActivatedRoute);
   protected member = signal<Member | undefined>(undefined);
-  protected editableMember: EditableMember={
+  protected editableMember: EditableMember = {
     displayName: '',
     description: '',
     city: '',
     country: '',
-  }
+  };
 
   ngOnInit(): void {
     this.route.parent?.data.subscribe((data) => {
@@ -38,9 +51,13 @@ export class MemberProfile implements OnInit, OnDestroy {
   updateProfile() {
     if (!this.member()) return;
     const updatedMember = { ...this.member(), ...this.editableMember };
-    console.log(updatedMember);
-    this.toast.success('Profile updated successfully');
-    this.memberService.editMode.set(false);
+    this.memberService.updateMember(this.editableMember).subscribe({
+      next: () => {
+        this.toast.success('Profile updated successfully');
+        this.memberService.editMode.set(false);
+        this.editForm?.reset(updatedMember);
+      },
+    });
   }
 
   ngOnDestroy(): void {
