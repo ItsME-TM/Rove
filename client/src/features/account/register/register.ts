a8b9ce16c87cb8f1ef334cf0e11f694cec07d98c
ItsME-TM <thushanmalaka123@gmail.com>
2026-07-02 22:13:02 +0530

    feat: implement reactive form for registration with validation

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,15 +1,16 @@
-import { Component, inject, input, output } from '@angular/core';
+import { Component, inject, input, OnInit, output } from '@angular/core';
 import { RegisterCreds, User } from '../../../types/user';
-import { FormsModule } from "@angular/forms";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 import { AccountService } from '../../../core/services/account-service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {
+export class Register implements OnInit{
   // Modern input function (Angular 17+) to receive data from parent component (Home)
   // .required() means this input must be provided by the parent
   // This does the same thing as @Input() decorator but in a more concise way
@@ -18,15 +19,29 @@ export class Register {
   // Output event emitter to send data to parent component
   cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds;
+  protected registerForm: FormGroup = new FormGroup({});
+  
+  ngOnInit(): void {
+    this.initializeForm();
+  }
 
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      email: new FormControl(),
+      displayName: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    })
+  }
   register(){
-    this.accountService.register(this.creds).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: error => console.log(error)
-    });
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.creds).subscribe({
+    //   next: response => {
+    //     console.log(response);
+    //     this.cancel();
+    //   },
+    //   error: error => console.log(error)
+    // });
   }
 
   cancel() {
